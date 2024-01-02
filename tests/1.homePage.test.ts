066eadd08c4dd5cd610a7952f43b69cb7e3078c2
Aldijana Čulezović <116015661+aldijanaa@users.noreply.github.com>
2024-01-02 23:15:05 +0100

    Add files via upload

---
+++ tests/1.homePage.test.ts
@@ -0,0 +1,39 @@
+import { Builder, By, WebDriver } from "selenium-webdriver";
+import { createDriver, quitDriver } from "../core/config/driver-setup";   
+import { readFileSync } from "fs";
+import * as path from "path"; 
+
+import { HomePage } from "../core/page-objects/home-page";
+
+//NOTE: everything starts from home-page
+
+const dataFilePath = path.resolve(__dirname, "../core/data/data.json");
+const testData = JSON.parse(readFileSync(dataFilePath, "utf8"));
+
+let driver: WebDriver;
+let homePage: HomePage;  //ovo je predhodni productSearch
+
+
+
+beforeAll(async () => {
+    driver = await createDriver(testData.url.home_page);  //calling driver in this
+    homePage = new HomePage(driver);   //object of the class
+},10000);  //10 seconds for the driver to open
+
+
+//Regression Test 1. Product Search
+test("product search", async () => {
+    await homePage.acceptAllCookies();
+    //await homePage.clickOnXButton();   //discount pop-up (not showed)
+
+    await homePage.locateSearchBar();  //locate search bar
+    await homePage.enterInSearchBar();   //enter "sneakers" in search bar
+    await homePage.scrollDown();    //scroll down so we can see nicely products displayed
+    await homePage.validationPage();   //validate that correct page is displayed
+},10000);
+
+
+afterAll(async () => {
+    await quitDriver(driver);
+},20000);  //20 seconds for the driver to close
+
